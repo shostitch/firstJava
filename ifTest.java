aaa0d625fa05ce61c8e341d1416daf0b3428958c
“shostitch“ <stitchsho0325@gmail.com>
2022-08-30 16:51:35 +0900

    if条件分岐

---
+++ ifTest.java
@@ -0,0 +1,13 @@
+public class ifTest {
+  public static void main(String[] args){
+    int myAge = 39;
+    if(myAge == 35) {
+      System.out.println("私の年齢は３５歳！");
+    } else if(myAge >= 40){
+      System.out.println("私の年齢は４０歳以上です！");
+    } else {
+      System.out.println("私の年齢は３５歳でも４０歳以上でもありません！");
+    }
+    System.out.println("私の出身地は・・・");
+  }
+}

8a5ce69ee2a348882be23178d8487a913aaa9398
“shostitch“ <stitchsho0325@gmail.com>
2022-08-31 16:35:20 +0900

    追記

--- forTestDouble.java
+++ forTestDouble.java
@@ -1,8 +1,8 @@
 public class forTestDouble {
   public static void main(String[] args) {
-    for (int i = 1; i <= 5; i++){
+    for (int i = 1; i <= 5; i++) {
       System.out.print("[i:" + i + "] ");
-      for (int j =1; j <= 2; j++){
+      for (int j = 1; j <= 2; j++) {
         System.out.print("j:" + j + " ");
       }
       System.out.println();
